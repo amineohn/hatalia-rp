c055585979404b241d1339cc9329403b5bad596d
isveny <amineprojet7@gmail.com>
2022-01-25 21:57:06 +0100

    remove this webpack

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const { withSuperjson } = require("next-superjson");
 
 module.exports = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.node = {
-        net: "empty",
-      };
-    }
-
-    return config;
-  },
 };
 
 module.exports = withSuperjson()({});
